6ee6935a8d3b99be1e51d034939948e45048d733
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2025-04-05 23:06:42 +0300

    viktorija final works

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css';
+
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
